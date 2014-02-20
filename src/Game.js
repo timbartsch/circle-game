e633155be4d83c502e77dc5efe1883f47cf119e8
Tim Bartsch <timbartsch.de@gmail.com>
2014-02-20 18:50:18 +0100

    Added levels.

--- src/Game.js
+++ src/Game.js
@@ -25,6 +25,22 @@ BasicGame.Game = function (game) {
     this.background;
     this.circles;
     this.scoreText;
+    this.levelText;
+    this.misses;
+    this.level;
+
+    this.levels = [
+      { circles: 10, timeBetweenCircles: 1000 },
+      { circles: 12, timeBetweenCircles: 900 },
+      { circles: 14, timeBetweenCircles: 800 },
+      { circles: 16, timeBetweenCircles: 700 },
+      { circles: 18, timeBetweenCircles: 600 },
+      { circles: 20, timeBetweenCircles: 500 },
+      { circles: 22, timeBetweenCircles: 400 },
+      { circles: 24, timeBetweenCircles: 300 },
+      { circles: 26, timeBetweenCircles: 200 },
+      { circles: 28, timeBetweenCircles: 100 },
+    ];
 };
 
 BasicGame.Game.prototype = {
@@ -32,20 +48,60 @@ BasicGame.Game.prototype = {
 	create: function () {
 
 		//	Honestly, just about anything could go here. It's YOUR game after all. Eat your heart out!
+    
 
     BasicGame.score = 0;
+    this.misses = 0;
+    this.level = 0;
     this.background = this.game.add.sprite(0, 0, "background");
     this.background.inputEnabled = true;
     this.background.events.onInputUp.add(this.onBackgroundInputUp, this);
     this.scoreText = this.game.add.text(0, 0, "Score: " + BasicGame.score);
+    this.levelText = this.game.add.text(200, 0, "Level: " + this.level);
     this.circles = this.game.add.group();
-    this.game.time.events.repeat(Phaser.Timer.SECOND, 3, this.createCircle, this);
 
+    this.startLevel(0); 
 	},
+
+  startLevel: function(level){
+    this.level = level;
+
+    console.log("LEVEL", level);
+    
+    var circlesCount = this.levels[this.level].circles;
+    var timeBetweenCircles = this.levels[this.level].timeBetweenCircles;
+    var i;
+    var deadCircles = [];
+
+    this.levelText.setText("Level: " + level);
+
+    this.circles.forEachDead(function(circle, i){
+      deadCircles.push(circle);
+    }, this);
+
+    for(i=0; i < deadCircles.length; i++){
+      this.circles.remove(deadCircles[i]);
+    }
+
+    for(i = 0; i < circlesCount; i++){
+      this.game.time.events.add(timeBetweenCircles * (i + 1), this.createCircle, this);
+    }
+
+  },
+
+  startNextLevel: function(){
+    this.startLevel(this.level + 1);
+  },
 
 	update: function () {
 
 		//	Honestly, just about anything could go here. It's YOUR game after all. Eat your heart out!
+    
+
+    console.log(this.circles.countDead());
+    if(this.circles.countDead() === this.levels[this.level].circles) {
+      this.startNextLevel();
+    }
 
 	},
 
@@ -64,21 +120,42 @@ BasicGame.Game.prototype = {
 	},
 
   createCircle: function(){
-    var circle = this.circles.create(this.game.world.randomX, this.game.world.randomY, "circle");
+    var circle, circleGrowTween, circleShrinkTween;
+    
+    circle = this.circles.create(this.game.world.randomX, this.game.world.randomY, "circle");
     circle.inputEnabled = true;
     circle.input.pixelPerfect = true;
     circle.events.onInputUp.add(this.onCircleInputUp, this);
     circle.anchor.setTo(0.5, 0.5);
     circle.scale.setTo(0, 0);
-    this.game.add.tween(circle.scale)
-      .to({x: 1, y: 1}, 1000, Phaser.Easing.Linear.None)
-      .to({x: 0, y: 0}, 1000, Phaser.Easing.Linear.None)
-      .start();
+
+    circleGrowTween = this.game.add.tween(circle.scale).to({x: 1, y: 1}, 1000, Phaser.Easing.Linear.None);
+    circleShrinkTween = this.game.add.tween(circle.scale).to({x: 0, y: 0}, 1000, Phaser.Easing.Linear.None);
+    circle.game = this;
+    circleShrinkTween.onComplete.add(this.onCircleShrinkEnd, circle);
+    circleGrowTween.chain(circleShrinkTween);
+    circle.growTween = circleGrowTween;
+    circle.shrinkTween = circleShrinkTween;
+    circleGrowTween.start();
+  },
+
+  onCircleShrinkEnd: function(pointer){
+    var circle = this;
+
+    console.log("MISS");
+    circle.kill();
+    circle.game.updateScore(-5);
+    circle.game.misses += 1;
+    if(circle.game.misses > 3) {
+      circle.game.quitGame();
+    }
   },
 
   onCircleInputUp: function(circle, pointer){
-    this.updateScore(+5);
+    circle.growTween.stop();
+    circle.shrinkTween.stop();
     circle.kill();
+    this.updateScore(+5);
   },
 
   onBackgroundInputUp: function(background, pointer){
@@ -88,9 +165,6 @@ BasicGame.Game.prototype = {
   updateScore: function(scoreChange){
     BasicGame.score += scoreChange;
     this.scoreText.setText("Score: " + BasicGame.score);
-    if(BasicGame.score < 0) {
-      this.quitGame();
-    }
   }
 
 };
